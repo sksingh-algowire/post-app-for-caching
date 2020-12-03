4f3b810312a36f0e0ab8d200f3fa6292dfa4e60b
Shailaja Knat Singh <shailaja.s@algowire.com>
2020-12-03 19:34:34 +0530

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+for i in 1..30 do
+    begin
+        post = Post.new(title: "Post number #{i}", body: "Post body description #{i}")
+    
+        if post.save
+            comment = post.comments.build( name: "User #{i}", message: "Comment on post #{i} by user #{i}")
+            comment.save
+        end
+    rescue => exception
+      puts "exception occure"
+    end
+end
